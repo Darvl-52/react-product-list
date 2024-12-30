25d5568ce0e885246461284f61a4c2820994ce5b
Darvl-52 <160394810+Darvl-52@users.noreply.github.com>
2024-12-30 18:12:07 +0300

    add build

--- src/pages/createProduct/page.tsx
+++
@@ -1,9 +0,0 @@
-import {CreateForm} from "../../widgets/CreateForm/CreateForm.tsx";
-
-export function CreateProduct () {
-    return (
-        <div>
-            <CreateForm/>
-        </div>
-    )
-}
